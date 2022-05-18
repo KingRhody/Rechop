d322a69da99fa4a70c1a7b6daa60ebf948951c2a
symoh27 <symohamed586@gmail.com>
2022-05-18 18:29:20 +0200

    Initialisation des listes de trajet des camions

--- RechopProject.py
+++ RechopProject.py
@@ -1,7 +1,5 @@
 import random
 
-index_list = list(range(0, 19))
-
 f = open("distances.txt")
 initial_distance_matrix = []
 for line in f:
@@ -11,33 +9,56 @@ for line in f:
 
 "print(initial_distance_matrix)"
 
-matrix_path, c1_path, c2_path, c3_path = [], [], [], []
-random_numbers = random.sample(range(1, 20), 3)
-random_init_path = [random_numbers[0], random_numbers[1], random_numbers[2]]
+matrix_path, c1_path, c2_path, c3_path, c1_index, c2_index, c3_index, = [], [], [], [], [0], [0], [0]
+random_init_path = random.sample(range(1, 19), 3)
+index_path_list = list(range(1, 20))
 
 
-def first_path():
-    """Genration des trajets entre la banque et les 3 premieres communes"""
-    c1_path.append(initial_distance_matrix[random_init_path[0]][0])
-    c2_path.append(initial_distance_matrix[random_init_path[1]][0])
-    c3_path.append(initial_distance_matrix[random_init_path[2]][0])
-    print(random_numbers)
-    print(c1_path, c2_path, c3_path)
+def generate_index_path(c_path_index_list, len_list):
+    """Generation des indices de commune à traverser par un camion c"""
+    while len(index_path_list) != 0 and len(c_path_index_list) != len_list:
+        index = random.randint(0, len(index_path_list)-1)
+        if index_path_list[index] not in random_init_path:
+            c_path_index_list.append(index_path_list[index])
+        index_path_list.remove(index_path_list[index])
 
 
-def initial_path():
-    """Genration des trajets de chaque camion dans une matrice"""
-    first_path()
-    i = 1
-    length = len(initial_distance_matrix)
-    while i < 20:
+def c_index_init():
+    """Initialisation des indices de commune à traverser par les trois camions"""
+    c1_index.append(random_init_path[0])
+    c2_index.append(random_init_path[1])
+    c3_index.append(random_init_path[2])
+
+    generate_index_path(c1_index, 7)
+    generate_index_path(c2_index, 7)
+    generate_index_path(c3_index, 8)
+
+    print(random_init_path)
+    print(c1_index, c2_index, c3_index)
+
+
+c_index_init()
+
+
+def generate_dist_path():
+    """length = len(initial_distance_matrix)
+    for i in range(20):
         if (i not in random_init_path) and (len(c1_path)) < 6:
             c1_path.append(initial_distance_matrix[i][-2])
         elif (i not in random_init_path) and ((6 <= i < 2*len(c1_path)) or (len(c2_path) < 6)):
             c2_path.append(initial_distance_matrix[i][-2])
         elif (2*len(c1_path) <= i < length) and (i not in random_init_path):
-            c3_path.append(initial_distance_matrix[i][-2])
-        i += 1
+            c3_path.append(initial_distance_matrix[i][-2])"""
+
+
+def initial_path():
+    """Genration des trajets entre la banque et 3 premieres communes"""
+
+    c1_path.append(initial_distance_matrix[c1_index[1]][0])
+    c2_path.append(initial_distance_matrix[c2_index[1]][0])
+    c3_path.append(initial_distance_matrix[c3_index[1]][0])
+
+    print(c1_path, c2_path, c3_path)
 
 
 def fill_matrix_path():
@@ -48,4 +69,4 @@ def fill_matrix_path():
     print(matrix_path)
 
 
-fill_matrix_path()
+"fill_matrix_path()"
