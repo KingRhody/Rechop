26f41e9217c2d3a3f43c37de87dd430cd803f82b
Rhody <45152539+KingRhody@users.noreply.github.com>
2022-05-19 18:34:17 +0200

    Update RechopProject.py

--- RechopProject.py
+++ RechopProject.py
@@ -41,9 +41,17 @@ def calculate_path_dist_risk(all_index_list, current_matrix_path):
                 risk_foreach_path[current_index] += get_distance * amount_list[i[c]]
             c += 1
         current_index += 1
-    print(dist_foreach_path)
-    print(risk_foreach_path)
-    return None
+    distance, risk = calculate_distance_risk(dist_foreach_path,risk_foreach_path)
+    return distance, risk
+
+def calculate_distance_risk(distance_list, risk_list):
+    total_distance = 0
+    total_risk = 0
+    for i in range(3):
+        total_distance += distance_list[i]
+        total_risk += risk_list[i]
+
+    return total_distance, total_risk
 
 
 def get_total_distance(dist_list):
@@ -60,17 +68,28 @@ def c_index_init(current_matrix_index):
     generate_index_path(current_matrix_index[0], len_list1, index_path_list)
     generate_index_path(current_matrix_index[1], len_list2, index_path_list)
     generate_index_path(current_matrix_index[2], len_list3, index_path_list)
-    print(matrix_index)
+    return current_matrix_index
 
 
-def generate_solution(current_matrix_index, current_matrix_path):
+def generate_solution(current_matrix_index1, current_matrix_path1, current_matrix_index2, current_matrix_path2):
     """Genration des trajets entre la banque et 3 premieres communes"""
-    c_index_init(current_matrix_index)
-    calculate_path_dist_risk(current_matrix_index, current_matrix_path)
-    print(matrix_path)
+    matrix_index1 = c_index_init(current_matrix_index1)
+    matrix_index2 = c_index_init(current_matrix_index2)
+    distance1, risk1 = calculate_path_dist_risk(matrix_index1, current_matrix_path1)
+    distance2, risk2 = calculate_path_dist_risk(matrix_index2, current_matrix_path2)
+    print(distance2, risk2)
+    print(distance1
+          , risk1)
+    if distance1 < distance2 and risk1 < risk2:
+        best_distance, best_risk = distance1, risk1
+    elif distance2 < distance1 and risk2 < risk1:
+        best_distance, best_risk = distance2, risk2
+    return best_distance, best_risk
 
 
-matrix_index = [[], [], []]
-matrix_path = [[], [], []]
+matrix_index1 = [[], [], []]
+matrix_index2 = [[], [], []]
+matrix_path1 = [[], [], []]
+matrix_path2 = [[], [], []]
 
-generate_solution(matrix_index, matrix_path)
+print(generate_solution(matrix_index1, matrix_path1, matrix_index2, matrix_path2))
